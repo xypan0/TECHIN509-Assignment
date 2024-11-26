07e8a25b5a47df4e014e5e4336f72d576a94df6e
xypan0 <xxyy.ppp@qq.com>
2024-11-25 19:21:24 -0800

    update turtle_race.py

--- turtle_race.py
+++ turtle_race.py
@@ -12,7 +12,10 @@ bet = s.textinput(title="Make your Bet", prompt="Which turtle which win ? Enter 
 X = -230
 Y = -100
 colors = ["red", "orange", "yellow", "blue", "violet"]
-
+if bet not in colors:
+    bet = None
+    print("Invalid color!")
+    
 turtles = []
 for i in range(0, 5):
     t = Turtle(shape="turtle")
